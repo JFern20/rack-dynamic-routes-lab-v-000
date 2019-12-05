65ffc626137d3fe88263fa739318566f7020aaa3
Jonathan Fernandez <jfern20@gmail.com>
2019-12-05 01:14:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,8 +4,8 @@ class Application
   
   
   def call(env)
-    resp = Rack::Request.new(env) 
-    req = Rack::Response.new
+    req = Rack::Request.new(env) 
+    resp = Rack::Response.new
     
     if req.path.match(/items\/.+/)
      item_name = req.path.split("/items/").last
